90fc06de28cebc0662fc65157315faf03cc7d634
fmcurti <fmcurti@gmail.com>
2024-09-10 17:20:37 -0300

    fixes

--- record_dataset.py
+++ record_dataset.py
@@ -1,6 +1,5 @@
 import json
 
-
 import streamlit as st
 from st_audiorec import st_audiorec
 from st_files_connection import FilesConnection
@@ -29,16 +28,23 @@ def get_file_name(data_item):
     audios = list(filter(lambda x: x.startswith(f"finetunian/dataset/audios/{st.session_state.current_index}_{data_item['short_name']}"), audios))
     return f'{st.session_state.current_index}_{data_item["short_name"]}_{str(len(audios) + 1)}.wav'
 
-def save_audio(data_item, audio):
+def save_audio(data_item, audio, advance=False):
     global wav_audio_data
     file_name = get_file_name(data_item)
     with conn.fs.open(f"finetunian/dataset/audios/{file_name}", "wb") as f:
         f.write(audio)
     wav_audio_data = None
+    st.toast("Subidisimo papu gomez")
+    if advance:
+        advance_index()
     return file_name
 
-st.button("Previous", key="prev", on_click=lambda: advance_index(-1))
-st.button("Next", key="next", on_click=lambda: advance_index())
+prev_col, next_col = st.columns([1,1])
+
+with prev_col:
+    st.button("Previous", key="prev", on_click=lambda: advance_index(-1))
+with next_col:
+    st.button("Next", key="next", on_click=lambda: advance_index())
 
 data_item = dataset[st.session_state.current_index]
 
@@ -46,6 +52,11 @@ st.write(data_item["desc"])
 wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
-    st.button("Upload Audio", key="upload_audio", on_click=lambda: save_audio(data_item, wav_audio_data))
+    col1, col2 = st.columns([1,1])
+    with col1:
+        st.button("Upload Audio", key="upload_audio", on_click=lambda: save_audio(data_item, wav_audio_data))
+    with col2:    
+        st.button("Upload and Go Next", key="upload_advance", on_click=lambda: save_audio(data_item, wav_audio_data, True))
+    
     
 
