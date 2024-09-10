327064d7df35069c8a2e77eb9c1411d1d6a6e444
fmcurti <fmcurti@gmail.com>
2024-09-10 17:09:53 -0300

    demo

--- record_dataset.py
+++ record_dataset.py
@@ -1,34 +1,43 @@
 import json
-import io
-import os
 
-import librosa
-import soundfile as sf
+
 import streamlit as st
 from st_audiorec import st_audiorec
+from st_files_connection import FilesConnection
+
+conn = st.connection('s3', type=FilesConnection)
+
+def get_max_index():
+    audios = conn.fs.ls("finetunian/dataset/audios")
+    audios = list(filter(lambda x: x.endswith(".wav"), audios))
+    ids = list(map(lambda x: int(x.split("/")[-1].split("_")[0]), audios))
+    return max(ids) + 1 if len(ids) > 0 else 0
 
 dataset = json.load(open("dataset/data.json"))["sounds"]
 
-
 # Initialize the current index
 if "current_index" not in st.session_state:
-    st.session_state.current_index = 0
+    st.session_state.current_index = get_max_index()
     
-def advance_index():
-    st.session_state.current_index += 1
+def advance_index(n=1):
+    st.session_state.current_index += n
+    st.session_state.current_index = st.session_state.current_index % len(dataset)
+    
 
-def get_file_name(audio):
-    audios = os.listdir("dataset/audios")
-    audios = list(filter(lambda x: x.startswith(audio["short_name"]), audios))
-    return audio["short_name"] + "_" + str(len(audios) + 1) + ".wav"
+def get_file_name(data_item):
+    audios = conn.fs.ls("finetunian/dataset/audios")
+    audios = list(filter(lambda x: x.startswith(f"finetunian/dataset/audios/{st.session_state.current_index}_{data_item['short_name']}"), audios))
+    return f'{st.session_state.current_index}_{data_item["short_name"]}_{str(len(audios) + 1)}.wav'
 
-def save_audio(audio):
-    file_name = get_file_name(audio)
-    sf.write(f"dataset/audios/{file_name}", audio, 16000)
-    with open("dataset/audios.csv", "a") as f:
-        f.write(file_name + "," + audio["short_name"] + "\n")
+def save_audio(data_item, audio):
+    global wav_audio_data
+    file_name = get_file_name(data_item)
+    with conn.fs.open(f"finetunian/dataset/audios/{file_name}", "wb") as f:
+        f.write(audio)
+    wav_audio_data = None
     return file_name
 
+st.button("Previous", key="prev", on_click=lambda: advance_index(-1))
 st.button("Next", key="next", on_click=lambda: advance_index())
 
 data_item = dataset[st.session_state.current_index]
@@ -37,7 +46,6 @@ st.write(data_item["desc"])
 wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
-    audio, sr = librosa.load(io.BytesIO(wav_audio_data), sr=48000)
-    audio = librosa.resample(audio,orig_sr=48000,target_sr=16000)
-    st.button("Upload Audio", key="upload_audio", on_click=lambda: save_audio(audio))
+    st.button("Upload Audio", key="upload_audio", on_click=lambda: save_audio(data_item, wav_audio_data))
     
+
